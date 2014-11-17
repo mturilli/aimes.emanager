4a02b0cd5e2bff4a7ee991991ec4e00dafee61cb
mturilli <matteo.turilli@gmail.com>
2014-11-17 03:45:36 -0500

    Mail reports about Workload with more fitting subject

--- bin/demo_SC2014.sh
+++ bin/demo_SC2014.sh
@@ -17,6 +17,10 @@ fi
 # Get some information from the logs.
 SESSION_UID=`grep 'created        : UID' out.log | cut -d ' ' -f 13`
 RADICAL_PILOT_RESOURCES=`grep 'IDs: ' out.log | cut -d ':' -f 2 | sed 's/^ \(.*\)/\1/'`
+N_TASKS=`grep 'Total number of tasks' out.log | cut -d ':' -f 2 | sed 's/^ *\(.*\)/\1/'`
+N_STAGES=`grep 'Total number of stages' out.log | cut -d ':' -f 2 | sed 's/^ *\(.*\)/\1/'`
+I_DATA=`grep 'Total input data' out.log | cut -d ':' -f 2 | sed 's/^ *\(.*\)/\1/'`
+O_DATA=`grep 'Total output data' out.log | cut -d ':' -f 2 | sed 's/^ *\(.*\)/\1/'`
 
 # Produce diagrams and statistics for the run.
 radicalpilot-stats -m plot,stat -s $SESSION_UID > stats.out 2>/dev/null
@@ -39,9 +43,16 @@ Infrastructure
 Resources: ${RADICAL_PILOT_RESOURCES}
 DBURL: ${RADICAL_PILOT_DBURL}
 
+Worflow
+-------
+Total number of stages: ${N_STAGES}
+Total number of tasks: ${N_TASKS}
+Total input data: ${I_DATA}
+Total output data: ${O_DATA}
+
 EOL
 
 cat stats.out | sed -e '1,/plotting.../d' >> description.log
 
 # Send the e-mail with the information, stats, diagram of the run.
-cat description.log | mutt -a "${SESSION_UID}.png" -a "stats.out" -s "[Experiment] $WORKLOAD_BAG_SIZE tasks - Session UID $SESSION_UID" -- matteo.turilli@gmail.com,andre@merzky.net,shantenu.jha@rutgers.edu
+cat description.log | mutt -a "${SESSION_UID}.png" -a "stats.out" -s "[AIMES demo SC2014] $N_TASKS tasks - Session UID $SESSION_UID" -- matteo.turilli@gmail.com,andre@merzky.net,shantenu.jha@rutgers.edu
