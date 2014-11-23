5f723325f80ff30adc320fd62e9f2a5040caaef9
Andre Merzky <andre@merzky.net>
2014-11-23 01:11:02 +0100

    resource tag conversion

--- bin/demo_SC2014_script.py
+++ bin/demo_SC2014_script.py
@@ -503,6 +503,8 @@ print "%s \n" % resource_priority
 # required container.
 def uri_to_tag(resource):
 
+    resource = resource.replace ('_', '.')
+
     tag = ''
 
     if resource == 'blacklight.psc.xsede.org':
